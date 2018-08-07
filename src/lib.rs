45732af3b06b6786d1f0cd8f9e98cddc50b1dcfa
Robert Habermeier <rphmeier@gmail.com>
2018-08-07 16:17:02 +0200

    add tests and fix some minor bugs

--- src/lib.rs
+++ src/lib.rs
@@ -35,18 +35,12 @@ pub struct Precommit<H> {
 
 /// Chain context necessary for implementation of the finality gadget.
 pub trait Chain<H> {
-	/// Compute block to target for finality, which is some fraction (rounding up) of the way between
-	/// the base and the best s.push(hash.clone()); of that head.
-	fn target(&self, base: H) -> H;
-
 	/// Get the ancestry of a block up to but not including the base hash.
 	/// Should be in reverse order from `block`'s parent.
 	fn ancestry(&self, base: H, block: H) -> Option<Vec<H>>;
-
-	/// Get children of block.
-	fn children(&self, block: H) -> Vec<H>;
 }
 
+#[derive(Debug)]
 struct Entry<H> {
 	number: usize,
 	// ancestor hashes in reverse order, e.g. ancestors[0] is the parent
@@ -80,7 +74,7 @@ struct GhostTracker<H: Hash + Eq> {
 	ghost_leader: Option<(H, usize)>,
 }
 
-impl<H: Hash + Eq + Clone> GhostTracker<H> {
+impl<H: Hash + Eq + Clone + ::std::fmt::Debug> GhostTracker<H> {
 	fn new(base_hash: H, base_number: usize, threshold: usize) -> Self {
 		let mut entries = HashMap::new();
 		entries.insert(base_hash.clone(), Entry {
@@ -103,7 +97,7 @@ impl<H: Hash + Eq + Clone> GhostTracker<H> {
 	}
 
 	/// Insert a vote into the tracker.
-	fn insert<C: Chain<H>>(&mut self, vote_target: (H, usize), weight: usize, chain: &C) {
+	fn insert<C: Chain<H>>(&mut self, vote_target: (H, usize), _weight: usize, chain: &C) {
 		let (vote_hash, vote_number) = vote_target;
 
 		match self.find_containing_nodes(vote_hash.clone(), vote_number) {
@@ -117,7 +111,6 @@ impl<H: Hash + Eq + Clone> GhostTracker<H> {
 
 		// NOTE: after this point, there is guaranteed to be a node with
 		// hash `vote_hash`.
-		unimplemented!()
 	}
 
 	// attempts to find the containing node keys for the given hash and number.
@@ -168,9 +161,9 @@ impl<H: Hash + Eq + Clone> GhostTracker<H> {
 
 	// introduce a branch to given vote-nodes.
 	//
-	// `to_split` is a list of nodes with ancestor-edges containing the given ancestor.
+	// `descendents` is a list of nodes with ancestor-edges containing the given ancestor.
 	//
-	// This function panics if any member of `to_split` is not a vote-node
+	// This function panics if any member of `descendents` is not a vote-node
 	// or does not have ancestor with given hash and number OR if `ancestor_hash`
 	// is already a known entry.
 	fn introduce_branch(&mut self, descendents: Vec<H>, ancestor_hash: H, ancestor_number: usize) {
@@ -180,20 +173,21 @@ impl<H: Hash + Eq + Clone> GhostTracker<H> {
 
 			debug_assert!(entry.in_direct_ancestry(&ancestor_hash, ancestor_number).unwrap());
 
+			// example: splitting number 10 at ancestor 4
+			// before: [9 8 7 6 5 4 3 2 1]
+			// after: [9 8 7 6 5 4], [3 2 1]
+			// we ensure the `entry.ancestors` is drained regardless of whether
+			// the `new_entry` has already been constructed.
 			{
-				let new_entry = maybe_entry.get_or_insert_with(|| {
-					// example: splitting number 10 at ancestor 4
-					// before: [9 8 7 6 5 4 3 2 1]
-					// after: [9 8 7 6 5 4], [3 2 1]
-					let offset = entry.number.checked_sub(ancestor_number + 1)
-						.expect("this function only invoked with direct ancestors; qed");
+				let offset = entry.number.checked_sub(ancestor_number)
+					.expect("this function only invoked with direct ancestors; qed");
+				let new_ancestors = entry.ancestors.drain(offset..);
 
-					Entry {
-						number: ancestor_number,
-						ancestors: entry.ancestors.drain(offset..).collect(),
-						descendents: vec![],
-						cumulative_weight: 0,
-					}
+				let new_entry = maybe_entry.get_or_insert_with(move || Entry {
+					number: ancestor_number,
+					ancestors: new_ancestors.collect(),
+					descendents: vec![],
+					cumulative_weight: 0,
 				});
 
 				new_entry.descendents.push(descendent);
@@ -230,7 +224,8 @@ impl<H: Hash + Eq + Clone> GhostTracker<H> {
 			chain returns ancestry only if the block is a descendent of base; qed");
 
 		let ancestor_hash = ancestry[ancestor_index].clone();
-		ancestry.truncate(ancestor_index);
+		ancestry.truncate(ancestor_index + 1);
+
 		self.entries.insert(hash.clone(), Entry {
 			number,
 			ancestors: ancestry,
@@ -240,5 +235,126 @@ impl<H: Hash + Eq + Clone> GhostTracker<H> {
 
 		self.heads.remove(&ancestor_hash);
 		self.heads.insert(hash);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	const GENESIS_HASH: &str = "genesis";
+	const NULL_HASH: &str = "NULL";
+
+	struct BlockRecord {
+		number: usize,
+		parent: &'static str,
+	}
+
+	struct DummyChain {
+		inner: HashMap<&'static str, BlockRecord>,
+	}
+
+	impl DummyChain {
+		fn new() -> Self {
+			let mut inner = HashMap::new();
+			inner.insert(GENESIS_HASH, BlockRecord { number: 1, parent: NULL_HASH });
+
+			DummyChain { inner }
+		}
+
+		fn push_blocks(&mut self, mut parent: &'static str, blocks: &[&'static str]) {
+			let base_number = self.inner.get(parent).unwrap().number + 1;
+			for (i, descendent) in blocks.iter().enumerate() {
+				self.inner.insert(descendent, BlockRecord {
+					number: base_number + i,
+					parent,
+				});
+
+				parent = descendent;
+			}
+		}
+	}
+
+	impl Chain<&'static str> for DummyChain {
+		fn ancestry(&self, base: &'static str, mut block: &'static str) -> Option<Vec<&'static str>> {
+			let mut ancestry = Vec::new();
+
+			loop {
+				match self.inner.get(block) {
+					None => return None,
+					Some(record) => { block = record.parent; }
+				}
+
+				ancestry.push(block);
+
+				if block == NULL_HASH { return None }
+				if block == base { break }
+			}
+
+			Some(ancestry)
+		}
+	}
+
+	#[test]
+	fn graph_fork_not_at_node() {
+		let mut chain = DummyChain::new();
+		let mut tracker = GhostTracker::new(GENESIS_HASH, 1, 7);
+
+		chain.push_blocks(GENESIS_HASH, &["A", "B", "C"]);
+		chain.push_blocks("C", &["D1", "E1", "F1"]);
+		chain.push_blocks("C", &["D2", "E2", "F2"]);
+
+		tracker.insert(("A", 2), 100, &chain);
+		tracker.insert(("E1", 6), 100, &chain);
+		tracker.insert(("F2", 7), 100, &chain);
+
+		assert!(tracker.heads.contains("E1"));
+		assert!(tracker.heads.contains("F2"));
+		assert!(!tracker.heads.contains("A"));
+
+		let a_entry = tracker.entries.get("A").unwrap();
+		assert_eq!(a_entry.descendents, vec!["E1", "F2"]);
+
+		let e_entry = tracker.entries.get("E1").unwrap();
+		assert_eq!(e_entry.ancestor_node().unwrap(), "A");
+
+		let f_entry = tracker.entries.get("F2").unwrap();
+		assert_eq!(f_entry.ancestor_node().unwrap(), "A");
+	}
+
+	#[test]
+	fn graph_fork_at_node() {
+		let mut chain = DummyChain::new();
+		let mut tracker1 = GhostTracker::new(GENESIS_HASH, 1, 7);
+		let mut tracker2 = GhostTracker::new(GENESIS_HASH, 1, 7);
+
+		chain.push_blocks(GENESIS_HASH, &["A", "B", "C"]);
+		chain.push_blocks("C", &["D1", "E1", "F1"]);
+		chain.push_blocks("C", &["D2", "E2", "F2"]);
+
+		tracker1.insert(("C", 4), 100, &chain);
+		tracker1.insert(("E1", 6), 100, &chain);
+		tracker1.insert(("F2", 7), 100, &chain);
+
+		tracker2.insert(("E1", 6), 100, &chain);
+		tracker2.insert(("F2", 7), 100, &chain);
+		tracker2.insert(("C", 4), 100, &chain);
+
+		for tracker in &[&tracker2] {
+			assert!(tracker.heads.contains("E1"));
+			assert!(tracker.heads.contains("F2"));
+			assert!(!tracker.heads.contains("C"));
+
+			let c_entry = tracker.entries.get("C").unwrap();
+			assert!(c_entry.descendents.contains(&"E1"));
+			assert!(c_entry.descendents.contains(&"F2"));
+			assert_eq!(c_entry.ancestor_node().unwrap(), GENESIS_HASH);
+
+			let e_entry = tracker.entries.get("E1").unwrap();
+			assert_eq!(e_entry.ancestor_node().unwrap(), "C");
+
+			let f_entry = tracker.entries.get("F2").unwrap();
+			assert_eq!(f_entry.ancestor_node().unwrap(), "C");
+		}
 	}
 }
